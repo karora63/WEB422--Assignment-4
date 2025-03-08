0ad2283efd86ef0fd7d626a0aa9094443f8b88ee
Khushi Arora <karora63myseneca.ca>
2025-03-07 22:56:59 -0500

    new

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ const Home = () =>   {
   return (
     <div>
       <Image
-        src="https://upload.wikimedia.org/wikipedia/commons/3/30/Metropolitan_Museum_of_Art_%28The_Met%29_-_Central_Park%2C_NYC.jpg""
+        src="https://upload.wikimedia.org/wikipedia/commons/3/30/Metropolitan_Museum_of_Art_%28The_Met%29_-_Central_Park%2C_NYC.jpg"
         alt="Metropolitan Museum of Art"
         width={1200}
         height={800}
